d36b6af0438bdc8a864322f4446fc754d0811c3e
Mark Spanbroek <mark@spanbroek.net>
2020-04-29 22:22:08 +0200

    Mark III layout

---
+++ keyboards/gh60/satan/keymaps/mark3/keymap.c
@@ -0,0 +1,48 @@
+#include QMK_KEYBOARD_H
+
+#define KC_____ KC_TRNS
+#define KC_FN1 MO(1) // Fn1
+#define KC_FN2 MO(2) // Fn2
+#define KC_HOME LGUI(KC_LEFT) // Home, or cmd-left
+#define KC_END LGUI(KC_RGHT) // End, or cmd-right
+#define KC_SLP LGUI(LALT(KC_POWER)) // sleep, or cmd-option-power
+#define KC_SCLK LCTL(LSFT(KC_POWER)) // lock screen, or ctrl-shift-power
+
+#define KC_SPFN LT(1, KC_SPC) // Space or Fn1
+
+#define KC_LCTO OSM(MOD_LCTL)
+#define KC_RCTO OSM(MOD_RCTL)
+#define KC_LATO OSM(MOD_LALT)
+#define KC_RATO OSM(MOD_RALT)
+#define KC_LGUO OSM(MOD_LGUI)
+#define KC_RGUO OSM(MOD_RGUI)
+#define KC_LSFO OSM(MOD_LSFT)
+#define KC_RSFO OSM(MOD_RSFT)
+
+// Mark II keyboard layout for GH60 Satan
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // 0: qwerty
+  [0] = LAYOUT_all(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_NO,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
+    KC_LSFT, KC_FN1,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_FN1,
+    KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPFN,                                     KC_RGUI, KC_RALT, KC_RCTL, KC_FN2
+  ),
+  // 1: fn1 layer
+  [1] = LAYOUT_all(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______,
+    KC_LSFO, _______, KC_LCTO, KC_LATO, KC_LGUO, _______, KC_SPC , _______, _______, KC_RGUO, KC_RATO, KC_RCTO, KC_RSFO, _______,
+    _______, _______, _______,                   _______,                                     _______, _______, _______, _______
+  ),
+  // 3: fn2 layer
+  [2] = LAYOUT_all(
+    KC_SLP , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLK, _______,
+    _______, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_VOLD, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,                   _______,                                     _______, _______, _______, _______
+  )
+};
