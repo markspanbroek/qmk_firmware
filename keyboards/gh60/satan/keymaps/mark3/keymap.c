80e123fabab7d5ea2580a5fc4c60bc9ddcbb1fb3
Mark Spanbroek <mark@spanbroek.net>
2020-08-06 00:55:40 +0200

    Split function layer and modifiers layer

--- keyboards/gh60/satan/keymaps/mark3/keymap.c
+++ keyboards/gh60/satan/keymaps/mark3/keymap.c
@@ -1,43 +1,50 @@
 #include QMK_KEYBOARD_H
 
-#define KC_____ KC_TRNS
-#define KC_FN1 MO(1) // Fn1
-#define KC_FN2 MO(2) // Fn2
-#define KC_SLP LGUI(LALT(KC_POWER)) // sleep, or cmd-option-power
-#define KC_SCLK LCTL(LSFT(KC_POWER)) // lock screen, or ctrl-shift-power
+#define KC_SPMD LT(1, KC_SPC) // Space or modifiers layer
+#define KC_FN   MO(2) // function layer
+#define KC_MDIA MO(3) // media layer
 
-#define KC_SPFN LT(1, KC_SPC) // Space or Fn1
+// left modifiers
+#define KC_LCTO MT(MOD_LCTL, KC_Z)
+#define KC_LATO MT(MOD_LALT, KC_X)
+#define KC_LGUO MT(MOD_LGUI, KC_C)
+#define KC_LFNO LT(2, KC_V)
 
-#define KC_LCTO OSM(MOD_LCTL)
-#define KC_RCTO OSM(MOD_RCTL)
-#define KC_LATO OSM(MOD_LALT)
-#define KC_RATO OSM(MOD_RALT)
-#define KC_LGUO OSM(MOD_LGUI)
-#define KC_RGUO OSM(MOD_RGUI)
-#define KC_LSFO OSM(MOD_LSFT)
-#define KC_RSFO OSM(MOD_RSFT)
+// right modifiers
+#define KC_RCTO MT(MOD_RCTL, KC_SLSH)
+#define KC_RATO MT(MOD_RALT, KC_DOT)
+#define KC_RGUO MT(MOD_RGUI, KC_COMM)
+#define KC_RFNO LT(2, KC_M)
 
 // Mark III keyboard layout for GH60 Satan
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // 0: qwerty
+  // qwerty
   [0] = LAYOUT_all(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_NO,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
-    KC_LSFT, KC_FN1,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_FN1,
-    KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPFN,                                     KC_RGUI, KC_RALT, KC_RCTL, KC_FN2
+    KC_LSFT, KC_FN,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_FN,
+    KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPMD,                                     KC_RGUI, KC_RALT, KC_RCTL, KC_MDIA
   ),
-  // 1: fn1 layer
+  // modifiers layer
   [1] = LAYOUT_all(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, KC_LCTO, KC_LATO, KC_LGUO, KC_LFNO, KC_SPC , _______, KC_RFNO, KC_RGUO, KC_RATO, KC_RCTO, _______, _______,
+    _______, _______, _______,                   _______,                                     _______, _______, _______, _______
+  ),
+  // function layer
+  [2] = LAYOUT_all(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______,
-    KC_LSFO, _______, KC_LCTO, KC_LATO, KC_LGUO, _______, KC_SPC , _______, _______, KC_RGUO, KC_RATO, KC_RCTO, KC_RSFO, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______,                   _______,                                     _______, _______, _______, _______
   ),
-  // 3: fn2 layer
-  [2] = LAYOUT_all(
-    KC_SLP , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLK, _______,
+  // media layer
+  [3] = LAYOUT_all(
+    KC_PWR , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SLEP, _______,
     _______, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, KC_VOLD, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
