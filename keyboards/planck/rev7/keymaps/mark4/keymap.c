88485c09c71e77fe496284ea9e79f19993fc5ecd
Mark Spanbroek <mark@spanbroek.net>
2024-05-19 16:32:58 +0200

    symmetric lower & raise on mark4

--- keyboards/planck/rev7/keymaps/mark4/keymap.c
+++ keyboards/planck/rev7/keymaps/mark4/keymap.c
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |Lower |Lower |    Space    |Raise |Raise | GUI  | Alt  | Ctrl |
+ * | Ctrl | Alt  | GUI  |Raise |Lower |    Space    |Lower |Raise | GUI  | Alt  | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI, LOWER,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   RAISE,   KC_RGUI, KC_RALT, KC_RCTL
+    KC_LCTL, KC_LALT, KC_LGUI, RAISE,   LOWER,   KC_SPC,  KC_SPC,  LOWER,   RAISE,   KC_RGUI, KC_RALT, KC_RCTL
 ),
 
 /* Movement layer (Lower)
