5c90ff3262a0486142ffbca938f5e5208c4d733b
Mark Spanbroek <mark@spanbroek.net>
2024-05-23 10:28:39 +0200

    move lower onto space bar on mark4

--- keyboards/planck/rev7/keymaps/mark4/keymap.c
+++ keyboards/planck/rev7/keymaps/mark4/keymap.c
@@ -18,7 +18,7 @@
 
 enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
 
-#define LOWER MO(_LOWER)
+#define LOWER LT(_LOWER, KC_SPC)
 #define RAISE MO(_RAISE)
 
 /* clang-format off */
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |Raise |Lower |    Space    |Lower |Raise | GUI  | Alt  | Ctrl |
+ * | Ctrl | Alt  | GUI  |      |Raise | Space/Lower |Raise |      | GUI  | Alt  | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI, RAISE,   LOWER,   KC_SPC,  KC_SPC,  LOWER,   RAISE,   KC_RGUI, KC_RALT, KC_RCTL
+    KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, RAISE,   LOWER,   LOWER,   RAISE,   XXXXXXX, KC_RGUI, KC_RALT, KC_RCTL
 ),
 
 /* Movement layer (Lower)
@@ -62,20 +62,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Number layer (Raise)
  * ,-----------------------------------------------------------------------------------.
- * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  |  \   |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |  10  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  '  |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F0  |  '  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | F11  | F12  |      |      |      |      |   -  |   =  |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |    Space    |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSLS,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT,
+    KC_GRV,  KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,   KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0,   KC_BSLS,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_QUOT,
     _______, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
