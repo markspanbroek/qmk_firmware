7bf1c9d3105ea06df280c085618f40f9e4ca0678
Mark Spanbroek <mark@spanbroek.net>
2024-06-01 08:02:38 +0200

    move - = to more natural position

--- keyboards/planck/rev7/keymaps/mark4/keymap.c
+++ keyboards/planck/rev7/keymaps/mark4/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F0  |  '  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | F11  | F12  |      |      |      |      |   -  |   =  |   [  |   ]  |      |
+ * |      | F11  | F12  |      |      |      |      |      |   -  |   =  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    Space    |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,   KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0,   KC_BSLS,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_QUOT,
-    _______, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+    _______, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  XXXXXXX, _______,
     _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______
 ),
 
